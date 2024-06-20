8f33048bcc3ba9d409cf890e26c5ecef5f38c910
chenh <1603812129@qq.com>
2024-06-20 23:10:35 +0800

    注册过滤器

--- src/main/java/com/tencent/wxcloudrun/filter/UserFilter.java
+++ src/main/java/com/tencent/wxcloudrun/filter/UserFilter.java
@@ -3,12 +3,14 @@ package com.tencent.wxcloudrun.filter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
 @Slf4j
+@WebFilter
 public class UserFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
